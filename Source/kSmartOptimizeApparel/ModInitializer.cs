e209b4731fb2269ab9d4977c2aadbdfa27b9da4e
koisama <koi@koi-sama.net>
2016-05-05 00:37:47 +0300

    initial commit

---
+++ Source/kSmartOptimizeApparel/ModInitializer.cs
@@ -0,0 +1,65 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace kSmartOptimizeApparel
+{
+    public class ModInitializer : ITab
+    {
+        protected GameObject modInitializerControllerObject;
+
+        public ModInitializer()
+        {
+            modInitializerControllerObject = new GameObject("ModInitializer");
+            modInitializerControllerObject.AddComponent<ModInitializerBehaviour>();
+            UnityEngine.Object.DontDestroyOnLoad((UnityEngine.Object)modInitializerControllerObject);
+        }
+
+        protected override void FillTab() { }
+    }
+
+    class ModInitializerBehaviour : MonoBehaviour
+    {
+        protected bool reinjectNeeded = false;
+        protected float reinjectTime = 0;
+
+        public void OnLevelWasLoaded(int level)
+        {
+            reinjectNeeded = true;
+            if (level >= 0)
+                reinjectTime = 1;
+            else
+                reinjectTime = 0;
+        }
+
+        public void FixedUpdate()
+        {
+            if (reinjectNeeded)
+            {
+                reinjectTime -= Time.fixedDeltaTime;
+
+                if (reinjectTime <= 0)
+                {
+                    reinjectNeeded = false;
+                    reinjectTime = 0;
+                }
+            }
+        }
+
+        public void Start()
+        {
+            MethodInfo coreMethod = typeof(JobGiver_OptimizeApparel).GetMethod("TryGiveTerminalJob", BindingFlags.Instance | BindingFlags.NonPublic);
+            MethodInfo moddedMethod = typeof(kJobGiver_OptimizeApparel).GetMethod("TryGiveTerminalJob", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            if (!CommunityCoreLibrary.Detours.TryDetourFromTo(coreMethod, moddedMethod))
+                Log.Error("kSmartOptimizeApparel: Error detouring mod.");
+
+            OnLevelWasLoaded(-1);
+        }
+    }
+}
---
+++ Source/kSmartOptimizeApparel/kJobGiver_OptimizeApparel.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse.AI;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+/*
+
+This is the modified copy of JobGiver_OptimizeApparel
+Changes include:
+ * Proper temperature curve - uses outdoor temperature for current month and real InsulationCold instead of base value
+ * Modified HP curve - pawns won't really mind worn out apparel as long as it's above 60% health
+ * Sharp and blunt protection values will use real values as well
+ * Apparel optimize check interval is increased and randomized in order to reduce CPU load
+
+*/
+
+namespace kSmartOptimizeApparel
+{
+    public class kJobGiver_OptimizeApparel
+    {
+        private const int ApparelOptimizeCheckInterval = 5500;
+
+        private const float MinScoreGainToCare = 0.05f;
+
+        private const float ScoreFactorIfNotReplacing = 10f;
+
+        private static float wantedWarmthTemperature = 0.1f;
+
+        private static StringBuilder debugSb;
+
+        private static SimpleCurve ColdCurve = new SimpleCurve
+        {
+            new CurvePoint(-100f, 1f),
+            new CurvePoint(0f, 0.1f)
+        };
+
+        private static readonly SimpleCurve HitPointsPercentScoreFactorCurve = new SimpleCurve
+        {
+            new CurvePoint(0f, 0.1f),
+            new CurvePoint(0.6f, 0.7f),
+            new CurvePoint(1f, 1f)
+        };
+
+        private void SetNextOptimizeTick(Pawn pawn)
+        {
+            pawn.mindState.nextApparelOptimizeTick = Find.TickManager.TicksGame + kJobGiver_OptimizeApparel.ApparelOptimizeCheckInterval + Rand.Range(1,5) * 101;
+        }
+
+        internal Job TryGiveTerminalJob(Pawn pawn)
+        {
+            if (pawn.outfits == null)
+            {
+                Log.ErrorOnce(pawn + " tried to run JobGiver_OptimizeApparel without an OutfitTracker", 5643897);
+                return null;
+            }
+            if (pawn.Faction != Faction.OfColony)
+            {
+                Log.ErrorOnce("Non-colonist " + pawn + " tried to optimize apparel.", 764323);
+                return null;
+            }
+            if (!DebugViewSettings.debugApparelOptimize)
+            {
+                if (Find.TickManager.TicksGame < pawn.mindState.nextApparelOptimizeTick)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                kJobGiver_OptimizeApparel.debugSb = new StringBuilder();
+                kJobGiver_OptimizeApparel.debugSb.AppendLine(string.Concat(new object[]
+                {
+                "Scanning for ",
+                pawn,
+                " at ",
+                pawn.Position
+                }));
+            }            
+
+            Outfit currentOutfit = pawn.outfits.CurrentOutfit;
+            List<Apparel> wornApparel = pawn.apparel.WornApparel;
+            for (int i = wornApparel.Count - 1; i >= 0; i--)
+            {
+                if (!currentOutfit.filter.Allows(wornApparel[i]) && pawn.outfits.forcedHandler.AllowedToAutomaticallyDrop(wornApparel[i]))
+                {
+                    return new Job(JobDefOf.RemoveApparel, wornApparel[i])
+                    {
+                        haulDroppedApparel = true
+                    };
+                }
+            }
+            Thing thing = null;
+            float num = 0f;
+            List<Thing> list = Find.ListerThings.ThingsInGroup(ThingRequestGroup.Apparel);
+            if (list.Count == 0)
+            {
+                this.SetNextOptimizeTick(pawn);
+                return null;
+            }
+
+            //determining temperature curve for current month for current pawn
+            wantedWarmthTemperature = pawn.def.GetStatValueAbstract(StatDefOf.ComfyTemperatureMin, null) - GenTemperature.AverageTemperatureAtWorldCoordsForMonth(Find.Map.WorldCoords, GenDate.CurrentMonth);
+            wantedWarmthTemperature = (wantedWarmthTemperature == 0f) ? 0.1f : wantedWarmthTemperature;
+
+            kJobGiver_OptimizeApparel.ColdCurve = new SimpleCurve
+                {
+                    new CurvePoint(wantedWarmthTemperature, 1f),
+                    new CurvePoint(wantedWarmthTemperature * 0.75f, 0.9f),
+                    new CurvePoint(0f, 0.5f)
+                };
+
+            for (int j = 0; j < list.Count; j++)
+            {
+                Apparel apparel = (Apparel)list[j];
+                if (currentOutfit.filter.Allows(apparel))
+                {
+                    if (Find.SlotGroupManager.SlotGroupAt(apparel.Position) != null)
+                    {
+                        if (!apparel.IsForbidden(pawn))
+                        {
+                            float num2 = kJobGiver_OptimizeApparel.ApparelScoreGain(pawn, apparel);
+                            if (DebugViewSettings.debugApparelOptimize)
+                            {
+                                kJobGiver_OptimizeApparel.debugSb.AppendLine(apparel.LabelCap + ": " + num2.ToString("F2"));
+                            }
+                            if (num2 >= kJobGiver_OptimizeApparel.MinScoreGainToCare && num2 >= num)
+                            {
+                                if (ApparelUtility.HasPartsToWear(pawn, apparel.def))
+                                {
+                                    if (pawn.CanReserveAndReach(apparel, PathEndMode.OnCell, pawn.NormalMaxDanger(), 1))
+                                    {
+                                        thing = apparel;
+                                        num = num2;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (DebugViewSettings.debugApparelOptimize)
+            {
+                kJobGiver_OptimizeApparel.debugSb.AppendLine("BEST: " + thing);
+                Log.Message(kJobGiver_OptimizeApparel.debugSb.ToString());
+                kJobGiver_OptimizeApparel.debugSb = null;
+            }
+            if (thing == null)
+            {
+                this.SetNextOptimizeTick(pawn);
+                return null;
+            }
+            return new Job(JobDefOf.Wear, thing);
+        }
+
+        public static float ApparelScoreGain(Pawn pawn, Apparel ap)
+        {
+            if (ap.def == ThingDefOf.Apparel_PersonalShield && pawn.equipment.Primary != null && !pawn.equipment.Primary.def.Verbs[0].MeleeRange)
+            {
+                return -1000f;
+            }
+            float num = kJobGiver_OptimizeApparel.ApparelScoreRaw(ap);
+            List<Apparel> wornApparel = pawn.apparel.WornApparel;
+            bool flag = false;
+            for (int i = 0; i < wornApparel.Count; i++)
+            {
+                if (!ApparelUtility.CanWearTogether(wornApparel[i].def, ap.def))
+                {
+                    if (!pawn.outfits.forcedHandler.AllowedToAutomaticallyDrop(wornApparel[i]))
+                    {
+                        return -1000f;
+                    }
+                    num -= kJobGiver_OptimizeApparel.ApparelScoreRaw(wornApparel[i]);
+                    flag = true;
+                }
+            }
+            if (!flag)
+            {
+                num *= kJobGiver_OptimizeApparel.ScoreFactorIfNotReplacing;
+            }
+            return num;
+        }
+
+        public static float ApparelScoreRaw(Apparel ap)
+        {
+
+            //base score
+            float score = 0.1f;
+
+            //calculating protection, it also gets a little buff
+            float protectionScore =
+                ap.GetStatValue(StatDefOf.ArmorRating_Sharp) +
+                ap.GetStatValue(StatDefOf.ArmorRating_Blunt) * 0.75f;
+            score += protectionScore * 1.25f;
+
+            //calculating HP
+            if (ap.def.useHitPoints)
+            {
+                float hpPercent = (float)ap.HitPoints / (float)ap.MaxHitPoints;
+                score *= kJobGiver_OptimizeApparel.HitPointsPercentScoreFactorCurve.Evaluate(hpPercent);
+            }
+
+            //calculating warmth
+            if (wantedWarmthTemperature > 5)
+            {
+                float warmth = Math.Abs(ap.GetStatValue(StatDefOf.Insulation_Cold, true));
+                score *= kJobGiver_OptimizeApparel.ColdCurve.Evaluate(warmth);
+            }
+
+            return score;
+        }
+    }
+}
